93eb29d2164cdc65a4f51df69a21ac413a33ea71
tolyaganzin <tolyaganzin@mail.ru>
2017-01-19 13:41:45 +0200

    add run app

--- app/app.js
+++ app/app.js
@@ -1,5 +1,16 @@
 angular
-  .module('YourApp', [
+  .module('app', [
     'ngMaterial',
     'ui.router'
   ]);
+  //run app
+  angular
+    .module('app')
+    .run(runApplication);
+
+  runApplication.$inject = ['$stateParams', '$rootScope', '$state'];
+
+  function runApplication($stateParams, $rootScope, $state) {
+      $rootScope.$state = $state;
+      $rootScope.$stateParams = $stateParams;
+  }
