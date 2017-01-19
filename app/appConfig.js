3f3717e77caaf9de6487dcda2d26d19a9bc4aacb
tolyaganzin <tolyaganzin@mail.ru>
2017-01-19 16:16:46 +0200

    add myTheme

--- app/appConfig.js
+++ app/appConfig.js
@@ -4,7 +4,30 @@ angular
 
 configApplication.$inject = ['$stateProvider', '$urlRouterProvider', '$httpProvider', '$mdThemingProvider'];
 
-function configApplication($stateProvider, $urlRouterProvider, $httpProvider) {
+function configApplication($stateProvider, $urlRouterProvider, $httpProvider, $mdThemingProvider) {
+  // Extend the red theme with a different color and make the contrast color black instead of white.
+  // For example: raised button text will be black instead of white.
+  var myPrimaryMap = $mdThemingProvider.extendPalette('blue');
+  var myAccentMap = $mdThemingProvider.extendPalette('light-blue');
+  var myWarnMap = $mdThemingProvider.extendPalette('yellow');
+
+  // Register the new color palette map with the name
+  $mdThemingProvider.definePalette('myPrimary', myPrimaryMap);
+  $mdThemingProvider.definePalette('myAccent', myAccentMap);
+  $mdThemingProvider.definePalette('myWarn', myWarnMap);
+
+  //Register 'myTheme'
+  // Use that theme for the primary intentions and accent
+  $mdThemingProvider.theme('myTheme')
+    .primaryPalette('myPrimary')
+    .accentPalette('myAccent')
+    .warnPalette('myWarn', {
+      'default': 'A700' // use shade A700 for default, and keep all other shades the same
+    });
+
+
+
+
   $urlRouterProvider.otherwise('/main');
   $stateProvider
     .state('main',{
