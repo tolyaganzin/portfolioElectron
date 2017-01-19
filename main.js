f79850510785fb0d89633135e75da984c7abc1fa
tolyaganzin <tolyaganzin@mail.ru>
2017-01-19 13:16:39 +0200

    add logo to window

--- main.js
+++ main.js
@@ -8,7 +8,11 @@ let win
 
 function createWindow () {
   // Create the browser window.
-  win = new BrowserWindow({width: 800, height: 600})
+  win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    icon: path.join(__dirname, 'logo.png')
+  })
 
   // and load the index.html of the app.
   win.loadURL(url.format({
