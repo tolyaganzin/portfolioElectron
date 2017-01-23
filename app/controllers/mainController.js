885de6a6d9ee471369cff795493d2436a1c7c0db
tolyaganzin <tolyaganzin@mail.ru>
2017-01-23 14:28:22 +0200

    add watcher to main controller

--- app/controllers/mainController.js
+++ app/controllers/mainController.js
@@ -2,13 +2,16 @@ angular
   .module('app')
   .controller('mainController', mainController);
 
-  mainController.$ingect = ['$mdMedia', '$mdSidenav'];
+  mainController.$ingect = ['$scope', '$mdMedia', '$mdSidenav'];
 
-  function mainController($mdMedia, $mdSidenav) {
+  function mainController($scope, $mdMedia, $mdSidenav) {
     var vm = this;
-
+    vm.showAndHide = !$mdMedia('gt-sm');
     vm.toggleLeft = buildToggler('left');
-    //vm.sideMenuToggle = sideMenuToggle;
+
+    $scope.$watch(function() { return $mdMedia('gt-sm'); }, function(sm) {
+      vm.showAndHide = sm;
+    });
 
     function buildToggler(componentId) {
       return function() {
